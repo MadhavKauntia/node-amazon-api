76fa9d2c4137dffddd24ff42c1d19348f92b7c8b
Madhav Kauntia <mkauntia@gmail.com>
2019-01-22 23:14:00 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+const scrape = require('./scrape/scrape')
+
+const url = 'https://www.amazon.in/dp/';
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/amazon/:id', (req, res) => {
+    var id = req.params.id;
+    const options = {
+        uri: url+id,
+        transform: function (body) {
+            return cheerio.load(body);
+        }
+    };
+    rp(options)
+        .then(($) => {
+            var productTitle = $('#productTitle').text().replace(/\n/gm, "").trim();
+            var price = $('#priceblock_dealprice').text().replace(/\n/gm, "").trim();
+            var rating = $('#acrPopover').attr('title');
+            var reviewsLink = 'https://www.amazon.in' + $('#acrCustomerReviewLink').attr('href');
+            var imageUrl = $('#landingImage').attr('data-old-hires');
+            var jsonFile = {productTitle, price, rating, reviewsLink, imageUrl};
+            res.send(jsonFile);
+        }, (e) => {
+            console.log('Error!');
+            return "What";
+        });
+});
+
+app.listen(port, () => {
+   console.log(`Port started on ${port}`);
+});
